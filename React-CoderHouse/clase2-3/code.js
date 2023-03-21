421f170874b9ae94e58ebaecbdd7a9188716c890
mateomeli <mateomeli7@gmail.com>
2023-03-21 20:44:03 -0300

    clase 3

---
+++ React-CoderHouse/clase2-3/code.js
@@ -0,0 +1,30 @@
+//SUGAR SYNTAX
+    //Ternario
+        // const edad = 13
+        // const mayor = edad >= 18 ? "nashe" : "nashen't"
+        // console.log(mayor)
+
+    //Destructuracion
+        // const arrayA = [1,3,4];
+        // const arrayB = [1,3,4];
+        // console.log([...arrayA,...arrayB])
+
+    //Pollyfills example
+        Array.prototype.customMap = function (callback){
+            
+            const arrayResult = [];
+            for (let index = 0; index < this.length; index++) {
+                const value = callback(this[index], index);
+                arrayResult.push(value);
+            }
+
+            return arrayResult;
+        };
+        const newArray = [1,2,5,6,9].customMap((value, index) => {
+            return ((index %2) === 0 ? value * 2 : value)
+        } );
+
+        // const oldArray = [1,2].map(value => value * 2);
+        // console.log(oldArray)
+
+        console.log(newArray)
